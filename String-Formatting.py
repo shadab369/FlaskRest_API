640a009f278bfea0a98912a8f96853f3442aa9bf
shadab369 <ansarishaah369@gmail.com>
2020-02-17 17:09:39 +0530

    done

--- String-Formatting.py
+++ String-Formatting.py
@@ -1,1 +1,27 @@
-print('Hello World')
+from flask import Flask
+from flask_restplus import Resource, Api
+from collections import Counter
+
+app = Flask(__name__)
+api = Api(app, title=' String Formatting', version='1.0',
+            description='A simple flask API for splitting and counting frequency of string')
+
+
+@api.route('/split_string/<string:String>/<int:Number>')
+class SplitString(Resource):
+    def get(self, String, Number):
+        String = String[::-1]
+        eml = []
+        for i in range(0, len(String), Number):
+            eml.append((String[i:i+Number])[::-1])
+        output = ' '.join(reversed(eml))
+        return {'Output Response': output}
+
+
+@api.route('/string_freq/<string:String>')
+class StringFreq(Resource):
+    def get(self, String):
+        rmspc = String.replace(' ', '')
+        spt = rmspc.split(',')
+        output = Counter(spt)
+        return {'Output Response': output}
